dd331acb2e5b9e98783d9ffbc665ff830910e368
abayoss <abayoss@yahoo.com>
2019-10-14 23:32:55 +0100

    Auth : web : login, logout, twig app.user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -20,7 +21,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/register", name="register")
+     * @Route("/register", name="security_register")
      */
     public function register(Request $request, ObjectManager $manager)
     {
@@ -45,4 +46,25 @@ class SecurityController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    { }
 }
